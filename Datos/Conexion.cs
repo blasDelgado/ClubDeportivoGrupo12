3ba7fe3ec731cf1d1225a02912834557b48a6b79
blasDelgado <blasdelgado10@gmail.com>
2023-11-28 15:09:53 -0300

    Se agrega la solicitud de dátos por teclado para la conexion a la bdd

--- Datos/Conexion.cs
+++ Datos/Conexion.cs
@@ -7,22 +7,57 @@ using System.Threading.Tasks;
 
 namespace ClubDeportivo.Datos
 {
-    internal class Conexion
+    public class Conexion
     {
+
         private string baseDatos;
-        private string servidor;
-        private string puerto;
-        private string usuario;
-        private string clave;
+        string servidor;
+        string puerto;
+        string usuario;
+        string clave;
         private static Conexion? con = null;
 
         private Conexion() // asignamos valores a las variables de la conexion
         {
+            bool correcto = false;
+            int mensaje;
+
+            string T_servidor = "Servidor";
+            string T_puerto = "Puerto";
+            string T_usuario = "Usuario";
+            string T_clave = "Clave";
+
+            while (correcto != true)
+            {
+                // Armamos los cuadros de dialogo para el ingreso de datos
+                T_servidor = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese servidor", "DATOS DE INSTALACIÓN MySQL");
+                T_puerto = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese puerto", "DATOS DE INSTALACIÓN MySQL");
+                T_usuario = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese usuario", "DATOS DE INSTALACIÓN MySQL");
+                T_clave = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese clave", "DATOS DE INSTALACIÓN MySQL");
+                mensaje = (int)MessageBox.Show("su ingreso: SERVIDOR = " +
+                T_servidor + " PUERTO= " + T_puerto + " USUARIO: " +
+                T_usuario + " CLAVE: " + T_clave,
+                "AVISO DEL SISTEMA", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+                if (mensaje != 6) // el valor 6 corresponde al SI
+                {
+                    MessageBox.Show("INGRESE NUEVAMENTE LOS DATOS");
+                    correcto = false;
+                }
+                else
+                {
+                    correcto = true;
+                }
+            }
             this.baseDatos = "ClubDeportivoGrupo12";
-            this.servidor = "localhost";
-            this.puerto = "3306";
-            this.usuario = "root";
-            this.clave = "";
+            this.servidor = T_servidor;
+            this.puerto = T_puerto;
+            this.usuario = T_usuario;
+            this.clave = T_clave;
         }
 
         // proceso de interacción
@@ -39,7 +74,7 @@ namespace ClubDeportivo.Datos
                 ";password=" + this.clave +
                 ";Database=" + this.baseDatos;
             }
-            catch 
+            catch
             {
                 cadena = null;
                 throw;
@@ -57,3 +92,4 @@ namespace ClubDeportivo.Datos
         }
     }
 }
+
